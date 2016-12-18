39fe839994ec5e818b959eb638a6948e3e5a8366
Adam Nemecek <adamnemecek@gmail.com>
2016-12-17 21:38:52 -0800

    stuff

--- Sources/SequenceTools.swift
+++ Sources/SequenceTools.swift
@@ -1,4 +1,41 @@
-struct SequenceTools {
 
-    var text = "Hello, World!"
+
+extension Sequence {
+    public func scan<T>(_ initial: T, _ combine: (T, Iterator.Element) throws -> T) rethrows -> [T] {
+        var accu = initial
+        return try map {
+            accu = try combine(accu, $0)
+            return accu
+        }
+    }
+}
+
+extension Sequence {
+    public func any(predicate: (Iterator.Element) -> Bool) -> Bool {
+        for e in self where predicate(e) {
+            return true
+        }
+        return false
+    }
+
+    public func all(predicate: (Iterator.Element) -> Bool) -> Bool {
+        for e in self where !predicate(e) {
+            return false
+        }
+        return true
+    }
+
+    public func none(predicate: (Iterator.Element) -> Bool) -> Bool {
+        for e in self where predicate(e) {
+            return false
+        }
+        return true
+    }
+
+}
+
+extension RangeReplaceableCollection where Iterator.Element: ExpressibleByIntegerLiteral {
+    init(zeros: Int) {
+        self.init(repeatElement(0, count: zeros))
+    }
 }
